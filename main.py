06c75457ad7a6a9121dd01a36be1a9ef28acba8a
Hans K <hans@communitycomputing.net>
2025-11-15 21:58:03 -0500

    WIP: The Rest Of The Stuff

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import opencollective
 import keyring
 from rich.console import Console
 from rich.prompt import Confirm, Prompt
+import pathlib
+import datetime
 
 app = typer.Typer()
 console = Console()
@@ -21,10 +23,46 @@ def __get_token():
     return token
 
 @app.command()
-def print_backers(org: Annotated[str, typer.Argument()] = "getsolus"):
+def print_backers(org: Annotated[str, typer.Option()] = "getsolus"):
     token = __get_token()
     client = opencollective.get_client(personal_token=token)
-    opencollective.get_backers(org=org, client=client)
+    opencollective.get_active_backers(org=org, client=client)
+
+@app.command()
+def list_backers(org: Annotated[str, typer.Option()] = "getsolus", tier: Annotated[str or None, typer.Argument()] = None):
+    token = __get_token()
+    client = opencollective.get_client(personal_token=token)
+    backers = opencollective.get_active_backers(org=org, client=client)
+    if tier:
+        backers = opencollective.filter_backers(backers, tier)
+        console.print(f"Found the following {len(backers)} backers for tier {tier} and organization {org}:", style="bold")
+    else:
+        console.print(f"Found the following {len(backers)} backers for all tiers on organization {org}:", style="bold")
+    for backer in backers:
+        console.print(backer["name"])
+
+@app.command()
+def list_tiers(org: Annotated[str, typer.Option()] = "getsolus"):
+    token = __get_token()
+    client = opencollective.get_client(personal_token=token)
+    tiers = opencollective.get_tiers(org=org, client=client)
+    console.print(f"The following tiers are available for the organization {org}:", style="bold")
+    for tier in tiers:
+        console.print(tier)
+
+@app.command()
+def export(
+        org: Annotated[str, typer.Argument()] = "getsolus",
+        tier: Annotated[str or None, typer.Option("--tier", "-t")] = None,
+        filename: Annotated[pathlib.Path, typer.Argument()] = f"./solus-backers{datetime.datetime.now().strftime("%m-%d-%Y")}.csv"
+):
+    token = __get_token()
+    client = opencollective.get_client(personal_token=token)
+    all_backers = opencollective.get_active_backers(client=client)
+
+
+
+
 
 @app.command()
 def set_token(token: Annotated[str or None, typer.Option(help="Open Collective Personal Token (optional - can be specified via CLI to keep the token out of shell history)")] = None):
--- opencollective.py
+++ opencollective.py
@@ -4,6 +4,11 @@ from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
 
 def get_client(personal_token: str or None = None):
+    """
+    Constructs a GraphQL Client object which can connect to Open Collective's API.
+    :param personal_token:
+    A personal token to use for authentication.
+    """
     if personal_token:
         transport = AIOHTTPTransport('https://api.opencollective.com/graphql/v2', headers={'Personal-Token': personal_token})
     else:
@@ -11,7 +16,16 @@ def get_client(personal_token: str or None = None):
     client = Client(transport=transport)
     return client
 
-def get_backers(client: Client, org:str = 'getsolus') -> None:
+def get_active_backers(client: Client, org:str = 'getsolus') -> list:
+    """
+    Retrieves active backers for an Open Collective organization using OC's GraphQL API.
+    :param org:
+    The slug for an Open Collective organization (defaults to "getsolus").
+    :param client:
+    A GraphQL Client object. See this module's "get_client" function.
+    :return:
+    A list of active backers for the given organization.
+    """
     query = gql("""
 query collective($slug: String, $limit: Int, $offset: Int) {
   collective(slug: $slug) {
@@ -39,7 +53,41 @@ query collective($slug: String, $limit: Int, $offset: Int) {
         offset += limit
         query.variable_values = {"slug": org, "limit": limit, "offset": offset}
         backers += client.execute(query)['collective']['contributors']['nodes']
-    print(result['collective']['contributors']['totalCount'])
-    print(len(backers))
-    for backer in backers:
-        print(backer['name'], backer['description'], backer['account']['emails'])
+    backers = [backer for backer in backers if backer["description"]]
+    return backers
+
+def filter_backers(backers: list, tier: str) -> list:
+    """
+    Filters a list of Open Collective backers based on their contribution tier.
+    :param backers:
+    The list of backers.
+    :param tier:
+    The tier name. Valid choices for a given organization may be acquired using this module's "get_tiers" function.
+    """
+    return [backer for backer in backers if backer["description"] == tier]
+
+def get_tiers(client: Client, org:str = 'getsolus') -> list:
+    """
+    Retrieves a list of valid contribution tiers for a given Open Collective organization.
+    :param client:
+    A GraphQL Client object. See this module's "get_client" function.
+    :param org:
+    The slug for an Open Collective organization (defaults to "getsolus").
+    """
+    query = gql("""
+query collective($slug: String) {
+  collective(slug: $slug) {
+    name
+    slug
+    tiers {
+      totalCount
+      nodes {
+        name
+      }
+    }
+  }
+}
+    """)
+    query.variable_values = {"slug": org}
+    result = client.execute(query)
+    return [tier["name"] for tier in result["collective"]["tiers"]["nodes"]]
