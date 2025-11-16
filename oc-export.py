0ecc385c9934bab4269c2b462d31e495b291187a
Hans Kelson <hans@communitycomputing.net>
2025-11-16 00:35:18 -0500

    Initial working version

---
+++ oc-export.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+import csv
+
+import typer
+from typing_extensions import Annotated
+from typing import List
+import opencollective
+import keyring
+from rich.console import Console
+from rich.prompt import Confirm, Prompt
+from rich.text import Text
+import pathlib
+import datetime
+
+app = typer.Typer()
+console = Console()
+error_console = Console(stderr=True, style="bold red")
+
+
+def __get_token():
+    """
+    Attempt to get an Open Collective token from the keyring; prompt the user if it isn't found.
+    """
+    token = keyring.get_password("opencollective-export", "token")
+    if not token:
+        console.print(
+            "Token not found in the system keyring. Please add one now.", style="yellow"
+        )
+        set_token()
+        token = keyring.get_password("opencollective-export", "token")
+    return token
+
+
+@app.command()
+def list_backers(
+    org: Annotated[str, typer.Option()] = "getsolus",
+    tier: Annotated[str or None, typer.Argument()] = None,
+):
+    token = __get_token()
+    client = opencollective.get_client(personal_token=token)
+    backers = opencollective.get_active_backers(org=org, client=client)
+    if tier:
+        backers = opencollective.filter_backers(backers, tier)
+        console.print(
+            f"Found the following {len(backers)} backers for tier {tier} and organization {org}:",
+            style="bold",
+        )
+    else:
+        console.print(
+            f"Found the following {len(backers)} backers for all tiers on organization {org}:",
+            style="bold",
+        )
+    for backer in backers:
+        console.print(backer["name"])
+
+
+@app.command()
+def list_tiers(org: Annotated[str, typer.Option()] = "getsolus"):
+    token = __get_token()
+    client = opencollective.get_client(personal_token=token)
+    tiers = opencollective.get_tiers(org=org, client=client)
+    console.print(
+        f"The following tiers are available for the organization {org}:", style="bold"
+    )
+    for tier in tiers:
+        console.print(tier)
+
+
+@app.command()
+def export(
+    org: Annotated[str, typer.Option()] = "getsolus",
+    tier: Annotated[List[str], typer.Argument()] = (),
+    base_filename: Annotated[
+        pathlib.Path, typer.Option()
+    ] = f"./solus-backers_{datetime.datetime.now().strftime("%m-%d-%Y")}.csv",
+):
+    token = __get_token()
+    client = opencollective.get_client(personal_token=token)
+    console.print("Querying OpenCollective (may take a moment)...")
+    all_backers = opencollective.get_active_backers(client=client, org=org)
+    if tier:
+        tiers = tier
+    else:
+        console.print("No tiers specified, using all available tiers.", style="yellow")
+        tiers = opencollective.get_tiers(org=org, client=client)
+    for tier in tiers:
+        backers = opencollective.filter_backers(
+            all_backers,
+            [
+                tier,
+            ],
+        )
+        filename = (
+            base_filename.parent / f"{base_filename.stem}-{tier.replace(' ', '_')}.csv"
+        ).with_suffix(".csv")
+        if filename.exists() and not Confirm.ask(
+            Text.assemble(
+                ("Warning:", "bold yellow"),
+                f' Output file "{filename}" aready exists. Overwrite it?',
+            )
+        ):
+            exit()
+        skipped_backers = []
+        with open(filename, "w", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            for backer in backers:
+                if len(backer["account"]["emails"]) < 1:
+                    skipped_backers.append(backer)
+                else:
+                    writer.writerow([backer["name"], backer["account"]["emails"][-1]])
+        console.print(f"Successfully exported {len(backers)} backers to {filename}.")
+        if skipped_backers:
+            console.print(
+                f"The following {len(skipped_backers)} backers were skipped; they lack email addresses:"
+            )
+            for backer in skipped_backers:
+                console.print(backer["name"])
+
+
+@app.command()
+def set_token(
+    token: Annotated[
+        str or None,
+        typer.Option(
+            help="Open Collective Personal Token (optional - can be specified via CLI to keep the token out of shell history)"
+        ),
+    ] = None,
+):
+    """
+    Adds your Open Collective token to the system keyring for future use.
+    """
+    if keyring.get_password("opencollective-export", "token"):
+        if not Confirm.ask("Overwrite existing token? "):
+            exit()
+    if not token:
+        token = Prompt.ask("Please enter your Open Collective Personal Token")
+    try:
+        keyring.set_password("opencollective-export", "token", token)
+    except keyring.errors.PasswordSetError:
+        error_console.print("Failed to save token: Keyring access error.")
+        exit()
+    if keyring.get_password("opencollective-export", "token") == token:
+        console.print("Successfully saved token.", style="green")
+
+
+if __name__ == "__main__":
+    app()
--- opencollective.py
+++ opencollective.py
@@ -3,6 +3,7 @@
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
 
+
 def get_client(personal_token: str or None = None):
     """
     Constructs a GraphQL Client object which can connect to Open Collective's API.
@@ -10,13 +11,19 @@ def get_client(personal_token: str or None = None):
     A personal token to use for authentication.
     """
     if personal_token:
-        transport = AIOHTTPTransport('https://api.opencollective.com/graphql/v2', headers={'Personal-Token': personal_token})
+        transport = AIOHTTPTransport(
+            "https://api.opencollective.com/graphql/v2",
+            headers={"Personal-Token": personal_token},
+        )
     else:
-        raise NotImplementedError('OAuth is not implemented. Please supply a personal token.')
+        raise NotImplementedError(
+            "OAuth is not implemented. Please supply a personal token."
+        )
     client = Client(transport=transport)
     return client
 
-def get_active_backers(client: Client, org:str = 'getsolus') -> list:
+
+def get_active_backers(client: Client, org: str = "getsolus") -> list:
     """
     Retrieves active backers for an Open Collective organization using OC's GraphQL API.
     :param org:
@@ -26,7 +33,8 @@ def get_active_backers(client: Client, org:str = 'getsolus') -> list:
     :return:
     A list of active backers for the given organization.
     """
-    query = gql("""
+    query = gql(
+        """
 query collective($slug: String, $limit: Int, $offset: Int) {
   collective(slug: $slug) {
     name
@@ -38,35 +46,60 @@ query collective($slug: String, $limit: Int, $offset: Int) {
         description
         account {
           emails
+          orders {
+            totalCount
+            nodes {
+              status
+              frequency
+              toAccount {
+                slug
+              }
+            }
+          }
         }
       }
     }
   }
 }
-    """)
+    """
+    )
     limit = 100
     offset = 0
     query.variable_values = {"slug": org, "limit": limit, "offset": offset}
     result = client.execute(query)
-    backers = result['collective']['contributors']['nodes']
-    while len(backers) < result['collective']['contributors']['totalCount']:
+    backers = result["collective"]["contributors"]["nodes"]
+    while len(backers) < result["collective"]["contributors"]["totalCount"]:
         offset += limit
         query.variable_values = {"slug": org, "limit": limit, "offset": offset}
-        backers += client.execute(query)['collective']['contributors']['nodes']
-    backers = [backer for backer in backers if backer["description"]]
-    return backers
+        backers += client.execute(query)["collective"]["contributors"]["nodes"]
+    output_backers = []
+    for backer in backers:
+        for order in backer["account"]["orders"]["nodes"]:
+            if (
+                order["status"] == "ACTIVE"
+                and order["frequency"] == "MONTHLY"
+                and order["toAccount"]["slug"] == org
+            ):
+                output_backers.append(backer)
+                break
+    return output_backers
 
-def filter_backers(backers: list, tier: str) -> list:
+
+def filter_backers(backers: list, tiers: list[str]) -> list:
     """
     Filters a list of Open Collective backers based on their contribution tier.
     :param backers:
     The list of backers.
-    :param tier:
-    The tier name. Valid choices for a given organization may be acquired using this module's "get_tiers" function.
+    :param tiers:
+    A list of tier names to select. Valid choices for a given organization may be acquired using this module's "get_tiers" function.
     """
-    return [backer for backer in backers if backer["description"] == tier]
+    output = []
+    for tier in tiers:
+        output += [backer for backer in backers if backer["description"] == tier]
+    return output
 
-def get_tiers(client: Client, org:str = 'getsolus') -> list:
+
+def get_tiers(client: Client, org: str = "getsolus") -> list:
     """
     Retrieves a list of valid contribution tiers for a given Open Collective organization.
     :param client:
@@ -74,7 +107,8 @@ def get_tiers(client: Client, org:str = 'getsolus') -> list:
     :param org:
     The slug for an Open Collective organization (defaults to "getsolus").
     """
-    query = gql("""
+    query = gql(
+        """
 query collective($slug: String) {
   collective(slug: $slug) {
     name
@@ -87,7 +121,8 @@ query collective($slug: String) {
     }
   }
 }
-    """)
+    """
+    )
     query.variable_values = {"slug": org}
     result = client.execute(query)
     return [tier["name"] for tier in result["collective"]["tiers"]["nodes"]]
