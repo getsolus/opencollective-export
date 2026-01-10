27f23d28c1e53f5f1a335bf156b960a9aabca374
Hans K <hans@communitycomputing.net>
2026-01-09 23:46:43 -0500

    .py files: add MPL 2.0 header

--- opencollective_export/opencollective.py
+++ opencollective_export/opencollective.py
@@ -1,5 +1,9 @@
 # Helper functions to access Open Collective's API
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
 
